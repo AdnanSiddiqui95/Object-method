f5b57a4d5bf49934659fa21f22aad9a8d5cb6a28
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-11 17:03:17 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,15 @@
+public class Age 
+{
+	public void ageA(int agevalue)
+	{
+		if(agevalue>20)
+		{
+			System.out.println("You can access the file");
+		}
+		else
+		{
+			System.out.println("You are not old enough to access");
+		}
+	}
+
+}
---
+++ AgeMain.java
@@ -0,0 +1,10 @@
+
+public class AgeMain {
+
+	public static void main(String[] args) {
+		Age Myobj=new Age();
+		Myobj.ageA(18);
+
+	}
+
+}

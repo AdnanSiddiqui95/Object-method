dcaca12056b75cefaa1c2445e701e665d36210be
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-11 16:52:26 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,13 @@
+public class AA 
+{
+	//Method called with the help of object in class AA 
+	public void Throttle() 
+	{
+	    System.out.println("The car is going as fast as it can");
+	}
+	public void Speed(int maxSpeed)
+	{
+		System.out.println("The maximum speed of the car is: " +maxSpeed);
+	}
+
+}
---
+++ AAk2.java
@@ -0,0 +1,12 @@
+public class AAk2 {
+
+	public static void main(String[] args) 
+	{
+		AA myObj=new AA();  // Create a myCar object 
+		myObj.Throttle();   // Call the fullThrottle() method
+		myObj.Speed(150);   // Call the speed() method
+		
+
+	}
+
+}

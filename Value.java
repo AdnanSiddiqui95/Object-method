6eb80cdba66cd5cabf16601a20a84e133403e8a6
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-11 17:15:45 +0530

    Add files via upload

---
+++ Value.java
@@ -0,0 +1,9 @@
+public class Value 
+{
+	public int add(int x ,int y)
+	{
+		System.out.print("the sum of two number is : ");
+		return x+y;
+	}
+
+}
---
+++ ValueMain.java
@@ -0,0 +1,11 @@
+
+public class ValueMain {
+
+	public static void main(String[] args) 
+	{
+		Value Obj=new Value();
+		int z= Obj.add(20,15);
+		System.out.println(z);
+	}
+
+}
